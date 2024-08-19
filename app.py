faaa38e71a5d9410c1f53240ee34756d68529ccc
0xtnpxsgt <66821987+0xtnpxsgt@users.noreply.github.com>
2024-08-19 18:47:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def get_inference(token):
 
     headers = {
         "accept": "application/json",
-        "x-cg-demo-api-key": "CG-nAPPCZX8GJabu1x36iKq7ePq"  # Replace with your API key
+        "x-cg-demo-api-key": "CG-XXXXXXXXXXXXXXXXXXX"  # Replace with your API key
     }
 
     response = requests.get(url, headers=headers)
